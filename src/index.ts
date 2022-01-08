62fba2ecf6b7375c3281ca680a4f651fd29ca222
moriwaki hikari <daiichidaimandaikichi.gi@gmail.com>
2022-01-08 14:18:40 +0900

    関数の使いかた

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 // import World from './world'
 
+
 // const root = document.getElementById('root')
 // const world = new World('Hello Wor!')
 // world.sayHello(root)
 
 //03.基本の型定義
-import {anySample, notExistSample, primitiveSample,unknownSample } from "./basic";
+// import {anySample, notExistSample, primitiveSample,unknownSample } from "./basic";
 
-anySample()
-notExistSample()
-primitiveSample()
-unknownSample()
+// anySample()
+// notExistSample()
+// primitiveSample()
+// unknownSample()
+
+//04.関数の形定義
+import { logMessage, logMessage5} from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+
+logMessage('Hello TypeScript!')
+logMessage5('Hello Type Signature!')
+isUserSignedIn('ABC', 'Hikari')
+isUserSignedIn('123')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(1000, 2000, 500, 3000, 250)
+console.log('Function parameters sample 5: Total price:', sum)
